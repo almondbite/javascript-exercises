ab8cbffa0418da85b869f2a0af3a8e3449eb5f36
almondbite <148507165+almondbite@users.noreply.github.com>
2024-09-12 15:53:26 -0500

    Cleaned up comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,4 @@
 const removeFromArray = function(array, ...ele) {
-    // const position = array.indexOf(ele);
-    // const remove = array.splice(position, 1);
-    
-
-    // for (i = array.length - 1; i >= 0; i--) {
-    //     array.splice(ele[i], 1);
-    // }
-    // return array;
-
-    // if (!Array.isArray(ele)) {
-    //     ele = [ele];
-    // }
-
-    // return array.filter(function(item) {
-    //     return !ele.includes(item);
-    // });
 
     return array.filter(item => !ele.includes(item));
 };
