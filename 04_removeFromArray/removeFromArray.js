5b178227e11bc9637858f479c293f9b584ec56d7
almondbite <148507165+almondbite@users.noreply.github.com>
2024-09-12 14:48:32 -0500

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...ele) {
+    // const position = array.indexOf(ele);
+    // const remove = array.splice(position, 1);
+    
+
+    // for (i = array.length - 1; i >= 0; i--) {
+    //     array.splice(ele[i], 1);
+    // }
+    // return array;
+
+    // if (!Array.isArray(ele)) {
+    //     ele = [ele];
+    // }
+
+    // return array.filter(function(item) {
+    //     return !ele.includes(item);
+    // });
+
+    return array.filter(item => !ele.includes(item));
 };
 
 // Do not edit below this line
