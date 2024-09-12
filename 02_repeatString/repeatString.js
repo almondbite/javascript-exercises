13330c4097ad40152d40f5c33ac310a346857370
almondbite <148507165+almondbite@users.noreply.github.com>
2024-09-12 13:30:48 -0500

    Completed exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let emptyStr = "";
+    if (num === -1) {
+        return "ERROR";
+    } else {
+    for (i = 0; i < num; i++) {
+        emptyStr += str;
+    }
+    return emptyStr;
+    }
 };
 
 // Do not edit below this line
