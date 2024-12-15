705e33236fb2ae6e332b85c057ea303202ebd0bd
almondbite <148507165+almondbite@users.noreply.github.com>
2024-12-14 19:02:36 -0600

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const conv = Math.round(((temp - 32) * (5/9)) * 10) / 10;
+  return conv;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const conv = Math.round((temp * (9/5) + 32) * 10) / 10;
+  return conv;
 };
 
 // Do not edit below this line
